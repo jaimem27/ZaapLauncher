60940cac9431b5894200b52ec58069185ae4090c
jaimem27 <jaime33m95@gmail.com>
2026-02-18 12:14:02 +0100

    Asset + animaciones
    Cambios en la ventana principal + animaciones en el portal

--- ZaapLauncher/ZaapLauncher.App/MainWindow.xaml.cs
+++ ZaapLauncher/ZaapLauncher.App/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace ZaapLauncher.App
 {
@@ -19,6 +13,50 @@ namespace ZaapLauncher.App
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new
+            {
+                ServerStatusText = "Offline",
+                ServerStatusColor = System.Windows.Media.Brushes.IndianRed,
+
+                ServerStatusGlowColor = System.Windows.Media.Colors.IndianRed,
+
+                NewsText = "• Patch notes will appear here.\n• Keep it short and clean.\n\nZaapLauncher v1.0 is initializing…",
+
+                StatusTitle = "Zaap node unreachable",
+                StatusSubtitle = "Check connection or try Repair."
+            };
+            Loaded += (_, __) =>
+            {
+                var breath = (Storyboard)FindResource("PortalBreath");
+                breath.Begin();
+
+                var vortex = (Storyboard)FindResource("VortexBrushRotate");
+                vortex.Begin();
+
+
+            };
+
+
         }
+
+        private void Header_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Discord_Click(object sender, RoutedEventArgs e)
+        {
+            var url = "https://discord.com/invite/drakonheart";
+
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+
     }
+
 }
